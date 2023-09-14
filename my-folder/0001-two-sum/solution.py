cc542c893245e90f1a594e395757cbab2aa4805c
Daniel Li <110048127+BananaApache@users.noreply.github.com>
2023-09-14 15:38:12 +0000

    [LeetCode Sync] Runtime - 1867 ms (40.16%), Memory - 14.2 MB (16.70%)

---
+++ my-folder/0001-two-sum/solution.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+
+        for (i, num1) in enumerate(nums):
+          for (j, num2) in enumerate(nums[i + 1 : ]):
+            if num1 + num2 == target:
+              return [ i, i + j + 1 ]
+            
+
