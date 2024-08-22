e17e1599b2ca998cd66fcf9ff5fcc5e7c6fdd5da
Daniel Li <110048127+BananaApache@users.noreply.github.com>
2024-08-22 00:08:49 +0000

    [LeetCode Sync] Runtime - 19 ms (1.16%), Memory - 16.6 MB (100.00%)

--- my-folder/0062-unique-paths/solution.py
+++ my-folder/0062-unique-paths/solution.py
@@ -1,12 +1,22 @@
 
-def factorial(n):
-    if n == 0 or n == 1:
-        return 1
-    else:
-        return n * factorial(n - 1)
+# def factorial(n):
+#     if n == 0 or n == 1:
+#         return 1
+#     else:
+#         return n * factorial(n - 1)
+
+def esteban_method(x):
+    product = 1
+
+    for i in range(1, x + 1):
+        product = product * i
+
+    return product
 
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        return int(factorial(m + n - 2) / (factorial(m - 1) * factorial(n - 1)))
+
+        # return int(factorial(m + n - 2) / (factorial(m - 1) * factorial(n - 1)))
+        return int(esteban_method(m + n - 2) / (esteban_method(m - 1) * esteban_method(n - 1)))
         
 
