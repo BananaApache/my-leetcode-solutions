5d336f8b61b8452484f0259977efd4914fcd9dd7
Daniel Li <110048127+BananaApache@users.noreply.github.com>
2024-08-22 22:02:09 +0000

    [LeetCode Sync] Runtime - 35 ms (7.41%), Memory - 16.7 MB (100.00%)

--- my-folder/0151-reverse-words-in-a-string/solution.py
+++ my-folder/0151-reverse-words-in-a-string/solution.py
@@ -1,10 +1,10 @@
-class Solution(object):
-    def reverseWords(self, s):
-        """
-        :type s: str
-        :rtype: str
-        """
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        sentence = []
+        s = list(reversed(s.split(" ")))
+        for word in s:
+            if not word.isspace() and word != "":
+                sentence.append(word)
+
+        return " ".join(sentence)
         
-        lst = s.split()
-        lst = lst[::-1]
-        return " ".join(lst)
