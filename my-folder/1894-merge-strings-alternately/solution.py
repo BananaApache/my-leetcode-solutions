551ef3db23f23b153225b16e553f13f35d16f1bd
Daniel Li <110048127+BananaApache@users.noreply.github.com>
2024-12-23 14:42:16 +0000

    [LeetCode Sync] Runtime - 22 ms (13.32%), Memory - 12.5 MB (0.78%)

--- my-folder/1894-merge-strings-alternately/solution.py
+++ my-folder/1894-merge-strings-alternately/solution.py
@@ -5,19 +5,16 @@ class Solution(object):
         :type word2: str
         :rtype: str
         """
-
-        output = []
-
-        for index in range(len(word1) + len(word2)):
-            try:
-                output.append(word1[index])
-            except:
-                pass
-            
-            try:
-                output.append(word2[index])
-            except:
-                pass
         
-        return "".join(output)
-        
+        output = ""
+
+        for index in range(max(len(word1), len(word2))):
+            if index >= len(word1):
+                output += word2[index]
+            elif index >= len(word2):
+                output += word1[index]
+            else:
+                output += word1[index]
+                output += word2[index]
+
+        return output
