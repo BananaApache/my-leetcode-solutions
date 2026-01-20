0d2573794dd1adf80fbbac179f2076a6043673c6
Daniel Li <110048127+BananaApache@users.noreply.github.com>
2026-01-20 01:04:39 +0000

    [LeetCode Sync] Runtime - 4 ms (40.93%), Memory - 21 MB (7.43%)

--- my-folder/0001-two-sum/solution.py
+++ my-folder/0001-two-sum/solution.py
@@ -1,14 +1,16 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        hashmap = {}
 
-        for (i, num1) in enumerate(nums):
-          for (j, num2) in enumerate(nums[i + 1 : ]):
-            if num1 + num2 == target:
-              return [ i, i + j + 1 ]
-            
+        for index in range(len(nums)): 
+            print(index)
 
+            difference = target - nums[index]
+
+            if hashmap.get(nums[index]) != None: # difference exists in hashmap
+                return [hashmap.get(nums[index]), index]
+            else: # difference not in hashmap, so add it
+                hashmap.update({difference : index})
+
+        print(hashmap)
