6e1a64c8bbcb51d109c35b1d5625a77d5b782b82
Daniel Li <110048127+BananaApache@users.noreply.github.com>
2026-01-25 19:39:28 +0000

    [LeetCode Sync] Runtime - 23 ms (5.08%), Memory - 59.9 MB (6.30%)

--- my-folder/0347-top-k-frequent-elements/solution.py
+++ my-folder/0347-top-k-frequent-elements/solution.py
@@ -1,19 +1,35 @@
+
+from collections import deque
+
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         
+        # [1,1,1,2,2,3], k = 2
+
+        # initialize a result array 
+        result = []
+
         freq = {}
 
         for index in range(len(nums)):
-            if freq.get(nums[index]):
-                freq[nums[index]] += 1
-            else:
-                freq[nums[index]] = 1
+            freq[nums[index]] = 1 + freq.get(nums[index], 0)
         
-        sorted_data = dict(sorted(freq.items(), key=lambda item: item[1]))
+        # fill the frequency hashmap
+        countMap = []
 
-        data = list(sorted_data.keys())
+        # fill countMap from 1..len(nums)
+        for index in range(len(nums)):
+            countMap.append(deque())
+        
+        for item, count in freq.items():
+            countMap[count - 1].append(item)
 
-        data.reverse()
+        # loop through the keys reversed
+        for q in countMap[::-1]:
+            # keep popping until q empty OR length of result is k
+            while len(q) != 0 and len(result) != k:
+                result.append(q.popleft())
 
-        return data[:k]
+        # return result array
+        return result
 
