4319d51f04bdf9a4d865df3929adb9bbd42326f4
Daniel Li <110048127+BananaApache@users.noreply.github.com>
2026-01-26 20:59:19 +0000

    [LeetCode Sync] Runtime - 11 ms (77.08%), Memory - 19.5 MB (7.45%)

--- my-folder/0003-longest-substring-without-repeating-characters/solution.py
+++ my-folder/0003-longest-substring-without-repeating-characters/solution.py
@@ -1,26 +1,47 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
 
-        if len(s) == 0:
-            return 0
-            
-        if len(s) == 1:
-            return 1
+        # edge cases:
+        # length 0 or 1
+        if len(s) == 0 or len(s) == 1:
+            return len(s)
 
-        leftPointer = 0
-        currLength = 1
+        # charSet = ab
+        # a b c d c c   length 6
+        # L R        
+        #                
+
+        # initializing
+        charSet = set()
+        left = 0
         maxLength = 1
-        
-        for rightPointer in range(1, len(s)):
-            
-            while s[rightPointer] in s[leftPointer : rightPointer]: 
-                # checking if next letter in substring already
-                leftPointer += 1
-            
-            currLength = rightPointer - leftPointer + 1
 
-            if currLength > maxLength:
-                maxLength = currLength
+        # add char at Left to charSet
+        charSet.add(s[left])
+
+        # keep moving Right index from 1..len(s)
+        for right in range(1, len(s)):
+            # but if char at Right in charSet -> we found a duplicate
+            if s[right] in charSet:
+                # remove char at Left from charSet
+                charSet.remove(s[left])
+                # increment Left
+                left += 1
+                # while the char at previous Left not equal to char at Right AND Left < Right
+                while s[left - 1] != s[right] and left < right:
+                    # remove char at Left from charSet
+                    charSet.remove(s[left])
+                    # increment Left
+                    left += 1
+
+                # add char at Right to charSet
+                charSet.add(s[right])
+            # else
+            else:
+                # add char at Right to charSet
+                charSet.add(s[right])
+                # set new maxLength
+                maxLength = max(maxLength, len(charSet))
 
         return maxLength
 
