a48021ea28aa7cc3a038339ea039d3f354e45b01
Daniel Li <110048127+BananaApache@users.noreply.github.com>
2026-01-29 16:24:33 +0000

    [LeetCode Sync] Runtime - 3 ms (33.49%), Memory - 19.4 MB (26.67%)

--- my-folder/0020-valid-parentheses/solution.py
+++ my-folder/0020-valid-parentheses/solution.py
@@ -1,29 +1,46 @@
 class Solution:
     def isValid(self, s: str) -> bool:
+
+        # edge case:
+        if len(s) == 1:
+            return False
+
+        # ({[]})
+
+        # (
+        # popped = {
+
+        # {{(
+        # 
         
+        # initializing
         stack = []
 
-        symbols = {
-            "(": ")",
-            "{": "}",
-            "[": "]"
+        bracketMap = {
+            "]": "[", 
+            ")": "(", 
+            "}": "{"
         }
 
-        s = list(s)
-
-        for char in s:
-            if char in symbols.keys():
-                stack.append(char)
-
-            if char in symbols.values():
-                if len(stack) > 0 and char == symbols[stack[-1]]:
-                    stack.pop()
-                else:
+        # loop through entire string
+        for index in range(len(s)):
+            # if current char is "[" or "(" or "{" -> current char is a left bracket
+            if s[index] in "[({":
+                # append to stack
+                stack.append(s[index])
+            # else -> current char is a right bracket
+            else:
+                if len(stack) == 0:
                     return False
-
-        print(stack)
-
+                # pop from stack
+                popped = stack.pop()
+                # if popped is not the opposite of current bracket
+                if popped != bracketMap[s[index]]:
+                    return False
+        
+        # if stack not empty
         if len(stack) != 0:
             return False
         else:
             return True
+
