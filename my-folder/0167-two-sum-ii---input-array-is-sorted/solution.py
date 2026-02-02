1ab50faa3f03cb5177b512ac6fea9fa93ed4de7a
Daniel Li <110048127+BananaApache@users.noreply.github.com>
2026-02-02 03:43:48 +0000

    [LeetCode Sync] Runtime - 3 ms (80.12%), Memory - 20.8 MB (7.62%)

--- my-folder/0167-two-sum-ii---input-array-is-sorted/solution.py
+++ my-folder/0167-two-sum-ii---input-array-is-sorted/solution.py
@@ -1,18 +1,27 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
 
+        # edge cases
+        if len(numbers) == 2:
+            return [1, 2]
+        
+        # init left = 0, right = len(numbers)
         left = 0
         right = len(numbers) - 1
-        
-        total = numbers[left] + numbers[right]
-        while total != target:
 
+        # keep looping while left smaller than right
+        while left < right:
+            total = numbers[left] + numbers[right]
+            # if sum bigger than target,    
             if total > target:
+                # decrement right because overshot
                 right -= 1
-                total = numbers[left] + numbers[right]
+            # if sum less than target,
             elif total < target:
+                # increment left because undershot
                 left += 1
-                total = numbers[left] + numbers[right]
-            
-        return [left + 1, right + 1]
+            # if sum equal,
+            else:
+                # return left + 1, right + 1
+                return [left + 1, right + 1]
 
