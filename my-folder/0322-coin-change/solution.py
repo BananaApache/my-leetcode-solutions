5055b69b45daaffb0f305f2bc3715a25c316ff51
Daniel Li <110048127+BananaApache@users.noreply.github.com>
2026-02-08 04:53:40 +0000

    [LeetCode Sync] Runtime - 443 ms (90.43%), Memory - 19.5 MB (77.91%)

--- my-folder/0322-coin-change/solution.py
+++ my-folder/0322-coin-change/solution.py
@@ -1,15 +1,15 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         
-        # initialize dp array
         dp = [amount + 1] * (amount + 1)
-        dp[0] = 0
 
-        for target in range(1, len(dp)):
+        for index in range(len(dp)):
+            if index == 0:
+                dp[index] = 0
+                continue
+
             for coin in coins:
-                if target - coin >= 0:
-                    dp[target] = min(dp[target], 1 + dp[target - coin])
+                if coin <= index:
+                    dp[index] = min(dp[index - coin] + 1, dp[index])
         
         return dp[amount] if dp[amount] != amount + 1 else -1
-
-
