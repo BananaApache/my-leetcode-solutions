f7fcde524ae2ee7fa47247bdc8a6c7b5f2a1885a
Daniel Li <110048127+BananaApache@users.noreply.github.com>
2026-02-09 04:59:29 +0000

    [LeetCode Sync] Runtime - 23 ms (47.81%), Memory - 19.5 MB (18.09%)

--- my-folder/0567-permutation-in-string/solution.py
+++ my-folder/0567-permutation-in-string/solution.py
@@ -1,47 +1,45 @@
-
-from collections import defaultdict
-
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         
-        # edge case:
-        if len(s1) > len(s2):
+        # edge case
+        if len(s2) < len(s1):
             return False
 
-        # initializing
-        s1Map = defaultdict(int)
-        for letter in s1:
-            s1Map[letter] += 1
+        alphabet = "abcdefghijklmnopqrstuvwxyz"
+        s1Freq = {}
+        s2Freq = {}
+        for letter in alphabet:
+            s1Freq[letter] = 0
+            s2Freq[letter] = 0
 
-        s2Map = defaultdict(int)
-        for letter in s2[0 : len(s1) - 1]: # only from s2 of index 0 to index length of s1 - 1
-            s2Map[letter] += 1
+        for index in range(len(s1)):
+            s1Freq[s1[index]] += 1
+            s2Freq[s2[index]] += 1
+
+        matches = 0
+        for letter in s1Freq.keys():
+            if s1Freq[letter] == s2Freq[letter]:
+                matches += 1
 
         left = 0
-        right = len(s1) - 1
-
-        # first loop through s2 substring one time to initialize while right < len(s2)
-        while right < len(s2):
-            s2Map[s2[right]] += 1
-            # if 2 dicts are equal
-            if s1Map == s2Map:
-                # then we found a match, return True
+        
+        for right in range(len(s1), len(s2)):
+            if matches == 26:
                 return True
-            
-            # if s2Map[char at left] is 1
-            if s2Map[s2[left]] == 1:
-                # remove s2Map[char at left]
-                del s2Map[s2[left]]
-            # else
-            else:
-                # decrement count of s2Map[char at left]
-                s2Map[s2[left]] = s2Map[s2[left]] - 1
-            # incremenet left
+
+            s2Freq[s2[right]] += 1
+            if s2Freq[s2[right]] == s1Freq[s2[right]]:
+                matches += 1
+            elif s2Freq[s2[right]] == s1Freq[s2[right]] + 1:
+                matches -= 1
+
+            s2Freq[s2[left]] -= 1
+            if s2Freq[s2[left]] == s1Freq[s2[left]]:
+                matches += 1
+            elif s2Freq[s2[left]] == s1Freq[s2[left]] - 1:
+                matches -= 1
             left += 1
 
-            # increment right
-            right += 1
-            # add char at right to s2Map
+        return matches == 26
 
-        # return False
-        return False
+
