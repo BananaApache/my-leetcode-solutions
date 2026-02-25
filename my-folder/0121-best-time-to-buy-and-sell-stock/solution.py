1efee98cfacb3dc9ee2b1253ef93a2ca31465dce
Daniel Li <110048127+BananaApache@users.noreply.github.com>
2026-02-25 22:34:03 +0000

    [LeetCode Sync] Runtime - 47 ms (60.62%), Memory - 28.7 MB (40.79%)

--- my-folder/0121-best-time-to-buy-and-sell-stock/solution.py
+++ my-folder/0121-best-time-to-buy-and-sell-stock/solution.py
@@ -1,26 +1,18 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
 
-        # [7,1,5,3,6,4] length 6
-        #  0 1 2 3 4 5
+        # [7,1,5,3,6,4]
+        #    1       2 
 
-        # edge cases
-        # length of prices is 1
-        if len(prices) == 1:
-            return 0
-
-        maxDifference = 0
-        # set left pointer to 0
         left = 0
-        # loop through entire array with right pointer but from 1..len(prices)
-        for right in range(1, len(prices)):
-            # check maxDifference
-            maxDifference = max(prices[right] - prices[left], maxDifference)
-            
-            # but if R - L < 0
-            if prices[right] - prices[left] < 0:
-                # set L = R
+        result = 0
+
+        for right in range(len(prices)):
+            profit = prices[right] - prices[left]
+            if profit >= 0:
+                result = max(result, profit)
+            else:
                 left = right
 
-        return maxDifference
+        return result
 
