345c5366754d8353c2a7eb536678bb091303d5e7
Daniel Li <110048127+BananaApache@users.noreply.github.com>
2026-03-19 00:56:30 +0000

    [LeetCode Sync] Runtime - 62 ms (27.53%), Memory - 28.6 MB (74.71%)

--- my-folder/0121-best-time-to-buy-and-sell-stock/solution.py
+++ my-folder/0121-best-time-to-buy-and-sell-stock/solution.py
@@ -1,18 +1,20 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-
+        
         # [7,1,5,3,6,4]
-        #    1       2 
+        #    1       2       
+        # result 5       
+
+        #     
 
         left = 0
         result = 0
 
         for right in range(len(prices)):
-            profit = prices[right] - prices[left]
-            if profit >= 0:
-                result = max(result, profit)
-            else:
+            if prices[right] < prices[left]:
                 left = right
+            else:
+                result = max(result, prices[right] - prices[left])
 
         return result
 
