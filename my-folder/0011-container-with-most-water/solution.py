0d6844440ceae702fde9ea44312ea001e80d69ae
Daniel Li <110048127+BananaApache@users.noreply.github.com>
2026-03-25 00:36:30 +0000

    [LeetCode Sync] Runtime - 66 ms (12.47%), Memory - 29.6 MB (37.05%)

--- my-folder/0011-container-with-most-water/solution.py
+++ my-folder/0011-container-with-most-water/solution.py
@@ -1,39 +1,25 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-
-        # [4, 3, 2, 7, 4, 9] len 6
-        #  0  1  2  3  4  5               
         
-        # initialize maxArea
-        maxArea = 0
+        result = 0
+        left = 0
+        right = len(height) - 1
+        
+        #  0 1 2 3 4 5 6 7 8     len 9
+        # [1,8,6,2,5,4,8,3,7]
+        #      1       2    
+        # result 49         
 
-        # initialize left column and right column
-        leftColumn = 0
-        rightColumn = len(height) - 1
+        while left < right:
+            area = min(height[left], height[right]) * (right - left)
+            result = max(area, result)
 
-        # keep looping until left < right
-        while leftColumn < rightColumn:
-            # now find that shorter column
-            shorterColumn = min(height[leftColumn], height[rightColumn])
-
-            # calculate area, adjust new max
-            maxArea = max(shorterColumn * (rightColumn - leftColumn), maxArea)
-
-            # if left column shorter, move left ...
-            # if both are equal, move left one
-            # move the shorter column until you find a column thats taller or equal
-            if height[leftColumn] <= height[rightColumn]:
-                # keep moving pointer
-                leftColumn += 1
-                while height[leftColumn] <= shorterColumn and leftColumn < rightColumn:
-                    leftColumn += 1
-                    # set that shorter column height to this new one
+            # left smaller
+            if height[left] <= height[right]:
+                left += 1
+            # right smaller
             else:
-                rightColumn -= 1
-                while height[rightColumn] <= shorterColumn and leftColumn < rightColumn:
-                    rightColumn -= 1
+                right -= 1
 
-
-        # return maxArea
-        return maxArea
+        return result
 
