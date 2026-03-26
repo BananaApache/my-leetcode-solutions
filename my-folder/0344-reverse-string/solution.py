9fdb7bcaad05148785de60e89abf37def246a9db
Daniel Li <110048127+BananaApache@users.noreply.github.com>
2026-03-26 00:29:47 +0000

    [LeetCode Sync] Runtime - 3 ms (46.18%), Memory - 23.4 MB (43.86%)

--- my-folder/0344-reverse-string/solution.py
+++ my-folder/0344-reverse-string/solution.py
@@ -3,24 +3,15 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-
-        # [1,2,3,4] even. len = 4
-        # [1,2]
-
-        # [1,2,3,4,5] odd. len = 5
-        # [1,2]
-
-        length = len(s)
-
-        for index in range(0, (length // 2)):
-            swap_position = length - 1 - index
-
-            tmp = ''
-
-            tmp = s[index]
-            s[index] = s[swap_position]
-            s[swap_position] = tmp
-
-
         
-        
+        left = 0
+        right = len(s) - 1
+
+        while left < right:
+            tmp = s[left]
+            s[left] = s[right]
+            s[right] = tmp
+
+            left += 1
+            right -= 1
+
