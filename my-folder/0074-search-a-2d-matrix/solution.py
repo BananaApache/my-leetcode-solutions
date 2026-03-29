531552c31964387aab16cb310141cc77c4a2795c
Daniel Li <110048127+BananaApache@users.noreply.github.com>
2026-03-29 23:43:26 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.6 MB (12.04%)

--- my-folder/0074-search-a-2d-matrix/solution.py
+++ my-folder/0074-search-a-2d-matrix/solution.py
@@ -1,33 +1,43 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        
+
+        """
+        target 3
+
+        [    0  1  2  3        
+        0   [1, 3, 5, 7],    tcb  
+        1   [10,11,16,20],    
+        2   [23,30,34,60]]    
+             l  m     r           
+        """
+
         top = 0
         bottom = len(matrix) - 1
 
         while top <= bottom:
-            middle = (bottom + top) // 2
-            if matrix[middle][-1] < target:
-                top = middle + 1
-            elif target < matrix[middle][0]:
-                bottom = middle - 1
-            else:
-                break
-        
-        if not (top <= bottom):
-            return False
+            center = (bottom + top) // 2
+            start = matrix[center][0] # start
+            end = matrix[center][-1] # end
 
-        left = 0
-        right = len(matrix[0]) - 1
-        row = (top + bottom) // 2
+            if start <= target and target <= end:
+                # do binary search
+                left = 0
+                right = len(matrix[center]) - 1
+                while left <= right:
+                    middle = (left + right) // 2
+                    if matrix[center][middle] < target: # move left
+                        left = middle + 1
+                    elif matrix[center][middle] > target: # move right
+                        right = middle - 1
+                    else:
+                        return True
+                
+                return False
 
-        while left <= right:
-            middle = (right + left) // 2
-            if matrix[row][middle] < target:
-                left = middle + 1
-            elif target < matrix[row][middle]:
-                right = middle - 1
+            elif target > end:
+                top = center + 1
             else:
-                return True
-        
+                bottom = center - 1
+
         return False
 
