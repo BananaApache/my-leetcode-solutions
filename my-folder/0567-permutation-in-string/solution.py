81268a3ad74a328844f90c1686344b0554533017
Daniel Li <110048127+BananaApache@users.noreply.github.com>
2026-03-31 04:46:45 +0000

    [LeetCode Sync] Runtime - 17 ms (64.35%), Memory - 19.3 MB (85.80%)

--- my-folder/0567-permutation-in-string/solution.py
+++ my-folder/0567-permutation-in-string/solution.py
@@ -1,45 +1,44 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        
+
         # edge case
-        if len(s2) < len(s1):
-            return False
+        if len(s1) > len(s2):
+            return False # s1 should always be smaller or equal to s2
 
-        alphabet = "abcdefghijklmnopqrstuvwxyz"
-        s1Freq = {}
-        s2Freq = {}
-        for letter in alphabet:
-            s1Freq[letter] = 0
-            s2Freq[letter] = 0
+        # s1 = "ab"
+        # s2 = "eidbaooo"
+        #       12    
 
-        for index in range(len(s1)):
-            s1Freq[s1[index]] += 1
-            s2Freq[s2[index]] += 1
+        # s1Map = {a: 1, b: 1}
+        # s2MovingMap = {e: 1, i: 1}
 
-        matches = 0
-        for letter in s1Freq.keys():
-            if s1Freq[letter] == s2Freq[letter]:
-                matches += 1
+        # decrement count of left from map, if 0 remove
+        # increment left
+        # increment right
+        # add right to map
+
+        s1Map = {}
+        s2Map = {}
+
+        for char1, char2 in zip(s1, s2):
+            s1Map[char1] = s1Map.get(char1, 0) + 1
+            s2Map[char2] = s2Map.get(char2, 0) + 1
 
         left = 0
-        
-        for right in range(len(s1), len(s2)):
-            if matches == 26:
+        right = len(s1) - 1
+        while right < len(s2):
+            if s1Map == s2Map:
                 return True
 
-            s2Freq[s2[right]] += 1
-            if s2Freq[s2[right]] == s1Freq[s2[right]]:
-                matches += 1
-            elif s2Freq[s2[right]] == s1Freq[s2[right]] + 1:
-                matches -= 1
-
-            s2Freq[s2[left]] -= 1
-            if s2Freq[s2[left]] == s1Freq[s2[left]]:
-                matches += 1
-            elif s2Freq[s2[left]] == s1Freq[s2[left]] - 1:
-                matches -= 1
+            if s2Map[s2[left]] == 1:
+                del s2Map[s2[left]]
+            else:
+                s2Map[s2[left]] -= 1
             left += 1
 
-        return matches == 26
+            right += 1
+            if right < len(s2):
+                s2Map[s2[right]] = s2Map.get(s2[right], 0) + 1
 
+        return False
 
