5bba61d9bb367d0754f1139ef003352b48ea1097
Daniel Li <110048127+BananaApache@users.noreply.github.com>
2026-05-19 00:31:57 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.2 MB (91.74%)

--- my-folder/0078-subsets/solution.py
+++ my-folder/0078-subsets/solution.py
@@ -1,25 +1,21 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         
+        # nums  1,2,3
+        # index 1
+
         result = []
 
-        subset = []
-        def dfs(index):
+        def dfs(root, index):
             # base case
-            if index >= len(nums): # index out of bounds
-                result.append(subset.copy())
+            if index == len(nums):
+                result.append(root)
                 return
 
-            # decision of adding nums[index]
-            subset.append(nums[index])
-            # then run dfs on next index
-            dfs(index + 1)
+            dfs(root + [nums[index]], index + 1)
+            dfs(root, index + 1)
 
-            # decision of not adding nums[index]
-            subset.pop()
-            # then run dfs on next index
-            dfs(index + 1)
-        
-        dfs(0)
+        dfs([], 0)
+
         return result
 
