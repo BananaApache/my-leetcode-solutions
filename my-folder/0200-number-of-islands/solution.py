224527e180938145ccfdd4dd97467572355d80ec
Daniel Li <110048127+BananaApache@users.noreply.github.com>
2026-06-09 14:22:57 +0000

    [LeetCode Sync] Runtime - 278 ms (12.59%), Memory - 21.6 MB (72.15%)

--- my-folder/0200-number-of-islands/solution.py
+++ my-folder/0200-number-of-islands/solution.py
@@ -1,33 +1,28 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        
+
         rows, cols = len(grid), len(grid[0])
+        result = 0
+        
+        def bfs(startRow, startCol):
+            q = deque( [(startRow, startCol)] )
+            grid[startRow][startCol] = "0"
 
-        islands = 0
-
-        def getNeighbors(row, col):
-            neighbors = []
-            directions = [ (1,0), (0,1), (-1,0), (0,-1) ]
-            for addRow, addCol in directions:
-                newRow, newCol = row + addRow, col + addCol
-                if newRow in range(rows) and newCol in range(cols):
-                    neighbors.append( (newRow, newCol) )
-            
-            return neighbors
-
-        def bfs(q):
             while q:
                 row, col = q.popleft()
-                for newRow, newCol in getNeighbors(row, col):
-                    if grid[newRow][newCol] == "1":
+
+                for addRow, addCol in [ [1,0],[0,1],[-1,0],[0,-1] ]:
+                    newRow, newCol = row + addRow, col + addCol
+
+                    if (newRow in range(rows) and newCol in range(cols)) and (grid[newRow][newCol] == "1"):
                         q.append( (newRow, newCol) )
-                        grid[newRow][newCol] = "X"
+                        grid[newRow][newCol] = "0"
 
         for row in range(rows):
             for col in range(cols):
                 if grid[row][col] == "1":
-                    bfs(deque([(row, col)]))
-                    islands += 1
+                    result += 1
+                    bfs(row, col)
 
-        return islands
+        return result
 
