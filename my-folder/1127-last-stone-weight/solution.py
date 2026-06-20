fd9ded25e82257eea25d2ee196e1e8fbf3262678
Daniel Li <110048127+BananaApache@users.noreply.github.com>
2026-06-20 17:36:50 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.5 MB (34.75%)

--- my-folder/1127-last-stone-weight/solution.py
+++ my-folder/1127-last-stone-weight/solution.py
@@ -1,20 +1,16 @@
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
-        
-        # build max heap
         maxHeap = stones
         heapq.heapify_max(maxHeap)
 
-        # stop looping when maxHeap has one element
         while len(maxHeap) > 1:
             stone1 = heapq.heappop_max(maxHeap)
             stone2 = heapq.heappop_max(maxHeap)
 
-            difference = stone1 - stone2
-
-            if difference != 0:
-                heapq.heappush_max(maxHeap, difference)
+            if stone1 != stone2:
+                heapq.heappush_max(maxHeap, stone1-stone2)
 
         return maxHeap[0] if maxHeap else 0
+            
 
-
+    
