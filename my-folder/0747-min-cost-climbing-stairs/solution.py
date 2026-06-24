35fcc13ba1569e6f94bd55242d254d45bf9c2f49
Daniel Li <110048127+BananaApache@users.noreply.github.com>
2026-06-24 16:50:07 +0000

    [LeetCode Sync] Runtime - 4 ms (30.67%), Memory - 19.3 MB (79.20%)

--- my-folder/0747-min-cost-climbing-stairs/solution.py
+++ my-folder/0747-min-cost-climbing-stairs/solution.py
@@ -1,10 +1,10 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        
+
         cost.append(0)
 
         for index in range(len(cost) - 3, -1, -1):
-            cost[index] += min(cost[index + 1], cost[index + 2])
+            cost[index] = min(cost[index] + cost[index + 1], cost[index] + cost[index + 2])
         
         return min(cost[0], cost[1])
 
