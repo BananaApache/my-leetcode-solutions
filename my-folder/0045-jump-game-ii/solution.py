aee57f32e237d91a5ae1ab59e774c221f3e99404
Daniel Li <110048127+BananaApache@users.noreply.github.com>
2026-06-29 14:38:58 +0000

    [LeetCode Sync] Runtime - 3 ms (89.27%), Memory - 20 MB (92.90%)

--- my-folder/0045-jump-game-ii/solution.py
+++ my-folder/0045-jump-game-ii/solution.py
@@ -1,17 +1,37 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
         
-        goal = len(nums) - 1
-        dp = [len(nums) + 1] * len(nums)
-        dp[-1] = 0
+        # GREEDY SOLUTION
+
+        #  0 1 2 3 4
+        # [2,3,1,1,4]
         
-        for index in range(len(nums) - 1, -1, -1):
-            if (nums[index] + index) >= goal:
-                for jump in range(index, min(nums[index] + index + 1, len(nums))):
-                    dp[index] = min(dp[index], 1 + dp[jump])
-                goal = index
+        result = 0
+        left = 0
+        right = 0
+
+        while right < len(nums) - 1:
+            maxJump = 0
+            for index in range(left, right + 1):
+                maxJump = max(maxJump, index + nums[index])
+            left = right + 1
+            right = maxJump
+            result += 1
         
-        print(dp)
+        return result
+
+        # DP SOLUTION
+        # goal = len(nums) - 1
+        # dp = [len(nums) + 1] * len(nums)
+        # dp[-1] = 0
         
-        return dp[0]
+        # for index in range(len(nums) - 1, -1, -1):
+        #     if (nums[index] + index) >= goal:
+        #         for jump in range(index, min(nums[index] + index + 1, len(nums))):
+        #             dp[index] = min(dp[index], 1 + dp[jump])
+        #         goal = index
+        
+        # print(dp)
+        
+        # return dp[0]
 
