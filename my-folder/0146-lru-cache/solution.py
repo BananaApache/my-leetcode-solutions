c560d62996e431a3a6864b74e9082a9ab7b5b2b0
Daniel Li <110048127+BananaApache@users.noreply.github.com>
2026-06-30 01:48:21 +0000

    [LeetCode Sync] Runtime - 137 ms (36.00%), Memory - 78.7 MB (25.49%)

--- my-folder/0146-lru-cache/solution.py
+++ my-folder/0146-lru-cache/solution.py
@@ -1,22 +1,20 @@
 
-class ListNode:
-    def __init__(self, key=None, val=None, prev=None, next=None):
+class Node:
+    def __init__(self, key=None, val=None):
         self.key = key
         self.val = val
-        self.prev = prev
-        self.next = next
+        self.prev = None
+        self.next = None
 
 class LRUCache:
-
     def __init__(self, capacity: int):
-        self.head = ListNode()
-        self.tail = ListNode()
+        self.capacity = capacity
+        self.hashmap = {} # (int) key -> (Node) node
+        self.head = Node()
+        self.tail = Node()
         self.head.next = self.tail
         self.tail.prev = self.head
-        self.hashmap = {}
-        self.capacity = capacity
 
-    # removes node from linked list
     def remove(self, node):
         prevNode = node.prev
         nextNode = node.next
@@ -24,40 +22,42 @@ class LRUCache:
         prevNode.next = nextNode
         nextNode.prev = prevNode
 
-    # appends node to end of linked list
-    def append(self, node):
-        prevNode = self.tail.prev
-        nextNode = self.tail
-        prevNode.next = nextNode.prev = node
+    def prepend(self, node):
+        prevNode = self.head
+        nextNode = self.head.next
+
+        prevNode.next = node
+        nextNode.prev = node
+
         node.next = nextNode
         node.prev = prevNode
 
     def get(self, key: int) -> int:
         if key in self.hashmap:
-            # update that key listnode to the most recently used (top of linked list)
             node = self.hashmap[key]
-
             self.remove(node)
-            self.append(node)
-
-            # return that value 
-            return self.hashmap[key].val
+            self.prepend(node)
+            return node.val
         else:
             return -1
-
+            
     def put(self, key: int, value: int) -> None:
         if key in self.hashmap:
-            self.remove(self.hashmap[key])
-        
-        self.hashmap[key] = ListNode(key, value)
-        self.append(self.hashmap[key])
+            # update it
+            newNode = self.hashmap[key]
+            newNode.val = value
+            self.remove(newNode)
+        else:
+            # add it
+            newNode = Node(key, value)
+            self.hashmap[key] = newNode
 
-        # check length of hashmap
-        if len(self.hashmap.keys()) > self.capacity: # LRUCache is at max capacity
-            # evict the head of linked list
-            lru = self.head.next
-            self.remove(lru)
-            del self.hashmap[lru.key]
+        self.prepend(newNode)
+
+        if len(self.hashmap) > self.capacity:
+            lastNode = self.tail.prev
+            self.remove(lastNode)
+            del self.hashmap[lastNode.key]
 
 
 # Your LRUCache object will be instantiated and called as such:
