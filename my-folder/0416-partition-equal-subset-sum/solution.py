f2dbe3749f5d3b07918bba8a9093e0990bb86149
Daniel Li <110048127+BananaApache@users.noreply.github.com>
2026-06-30 02:45:42 +0000

    [LeetCode Sync] Runtime - 217 ms (93.40%), Memory - 20 MB (60.85%)

--- my-folder/0416-partition-equal-subset-sum/solution.py
+++ my-folder/0416-partition-equal-subset-sum/solution.py
@@ -1,33 +1,53 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
         
-        # maybe sorted
-        # no two pointers
-        # sum NEEDS to be even
-        
-        # find if subset length (n - 1) can total to (sum / 2)
+        # DP Solution
 
-        # BRUTE FORCE IS DFS WITH BACKTRACK 2^(n-1)
-
-        # DP -> sum of all subsets up to index
-
-        numsTotal = sum(nums)
-        if numsTotal % 2 != 0: # odd sum
+        total = sum(nums)
+        if total % 2 != 0:
             return False
-        halfTotal = numsTotal // 2
 
-        possibleTotals = {0}
+        target = int(total / 2)
 
-        for num in nums:
-            nextPossible = set()
-            for total in possibleTotals:
-                newTotal = total + num
-                if newTotal == halfTotal:
+        possibleSums = set()
+        possibleSums.add(0)
+
+        for index in range(len(nums) - 1, -1, -1):
+            newSums = set()
+            for total in possibleSums:
+                newSum = total + nums[index]
+                if newSum == target:
                     return True
-                elif newTotal < halfTotal:
-                    nextPossible.add(newTotal)
+                elif newSum < target:
+                    newSums.add(newSum)
+            newSums.add(nums[index])
 
-            possibleTotals.update(nextPossible)
+            possibleSums.update(newSums)
 
         return False
+
+        # DFS backtracking decision tree solution
+
+        # total = sum(nums)
+        # if total % 2 != 0:
+        #     return False
+
+        # target = int(total / 2)
+
+        # def dfs(startIndex, total):
+        #     if total == target:
+        #         return True
+            
+        #     for index in range(startIndex + 1, len(nums)):
+        #         if dfs(index, total + nums[index]):
+        #             return True
+
+        #     return False
+
+        # for index in range(len(nums)):
+        #     if dfs(index, nums[index]):
+        #         return True
+        
+        # return False
+        
 
