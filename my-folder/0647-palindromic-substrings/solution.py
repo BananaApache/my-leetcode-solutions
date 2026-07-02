c688255c10052dc8ada4a10c7f6a3410662e18db
Daniel Li <110048127+BananaApache@users.noreply.github.com>
2026-07-02 01:39:11 +0000

    [LeetCode Sync] Runtime - 127 ms (52.19%), Memory - 19.2 MB (66.27%)

--- my-folder/0647-palindromic-substrings/solution.py
+++ my-folder/0647-palindromic-substrings/solution.py
@@ -2,21 +2,50 @@ class Solution:
     def countSubstrings(self, s: str) -> int:
 
         result = 0
-        
+
         for index in range(len(s)):
-            # odd case
+            # odd
             l, r = index, index
             while l >= 0 and r < len(s) and s[l] == s[r]:
                 result += 1
                 l -= 1
                 r += 1
-            
-            # even case
+
+            # even
             l, r = index, index + 1
             while l >= 0 and r < len(s) and s[l] == s[r]:
                 result += 1
                 l -= 1
                 r += 1
-
+        
         return result
+        
+        # def isP(left, right):
+        #     while left < right:
+        #         if s[left] != s[right]:
+        #             return False
+        #         left += 1
+        #         right -= 1
+            
+        #     return True
+
+        # result = 0
+        # seen = set()
+
+        # def dfs(left, right):
+        #     nonlocal result
+        #     # base case
+        #     if (left, right) not in seen and isP(left, right):
+        #         seen.add( (left, right) )
+        #         result += 1
+        #     if right >= len(s) - 1:
+        #         return
+
+        #     dfs(left + 1, right + 1)
+        #     dfs(left, right + 1)
+        
+        # dfs(0, 0)
+
+        # return result
+
 
