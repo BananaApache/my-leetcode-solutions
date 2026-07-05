8bd5ba82c6b96cf4e6bedfdc27af195ae8aa8990
Daniel Li <110048127+BananaApache@users.noreply.github.com>
2026-07-05 14:54:58 +0000

    [LeetCode Sync] Runtime - 1 ms (71.78%), Memory - 19.8 MB (28.30%)

--- my-folder/0684-redundant-connection/solution.py
+++ my-folder/0684-redundant-connection/solution.py
@@ -1,40 +1,78 @@
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
         
-        adjMap = { node : set() for node in range(1, len(edges) + 1) }
-        for a, b in edges:
-            adjMap[a].add(b)
-            adjMap[b].add(a)
-        
-        visited = set()
-        cycle = set()
-        cycleStart = None
-        cycleClosed = False
 
-        def isCycle(node, prev):
-            nonlocal cycleStart, cycleClosed
+        # union find with path compression and also ranking
+        parentMap = {node: node for node in range(1, len(edges) + 1)}
+        sizeMap = {node: 1 for node in range(1, len(edges) + 1)}
+
+        def find(node):
             # base case
-            if node in visited:
-                cycleStart = node
-                return True
+            # path compression part here
+            # basically sets each node to its 'root' parent immediately
+            if node == parentMap[node]:
+                return parentMap[node]
+            parentMap[node] = find(parentMap[node])
+            return parentMap[node]
+
+        for n1, n2 in edges:
+            p1 = find(n1)
+            p2 = find(n2)
+
+            if p1 == p2:
+                return [n1, n2]
+            else:
+                # union part
+                if sizeMap[p1] > sizeMap[p2]:
+                    # p2 smaller
+                    # attach p2 under p1
+                    parentMap[p2] = p1
+                    sizeMap[p1] += sizeMap[p2]
+                else:
+                    parentMap[p1] = p2
+                    sizeMap[p2] += sizeMap[p1]
+
+
+        # union find with path compression
+        # parentMap = {node: node for node in range(1, len(edges) + 1)}
+
+        # def find(node):
+        #     # base case
+        #     # path compression part here
+        #     # basically sets each node to its 'root' parent immediately
+        #     if node == parentMap[node]:
+        #         return parentMap[node]
+        #     parentMap[node] = find(parentMap[node])
+        #     return parentMap[node]
+
+        # for n1, n2 in edges:
+        #     p1 = find(n1)
+        #     p2 = find(n2)
+
+        #     if p1 == p2:
+        #         return [n1, n2]
+        #     else:
+        #         parentMap[p2] = p1 # union part
+        #         # parentMap[p1] = p2 ALSO WORKS, BUT UNION BY RANK OPTIMIZES THIS
+
+        # simple union find implementation
+        # parentMap = {node: node for node in range(1, len(edges) + 1)}
+
+        # def find(node):
+        #     # base case
+        #     if parentMap[node] == node:
+        #         return node
             
-            visited.add(node)
-            for neighbor in adjMap[node]:
-                if neighbor != prev and isCycle(neighbor, node):
-                    if not cycleClosed:
-                        cycle.add((node, neighbor))
-                        if node == cycleStart:
-                            cycleClosed = True
+        #     return find(parentMap[node])
+        
+        # for n1, n2 in edges:
+        #     p1 = find(n1)
+        #     p2 = find(n2)
 
-                    return True
+        #     if p1 == p2:
+        #         return [n1, n2]
+        #     else:
+        #         parentMap[p1] = p2
+
             
-            return False
-
-        isCycle(1, -1)
-
-        for index in range(len(edges) - 1, -1, -1):
-            a, b = edges[index]
-
-            if (a, b) in cycle or (b, a) in cycle:
-                return [a, b]
 
