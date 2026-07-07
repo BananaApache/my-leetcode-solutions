2e1f176013059f5e727f5e8d71deb832bc7697b1
Daniel Li <110048127+BananaApache@users.noreply.github.com>
2026-07-07 01:57:29 +0000

    [LeetCode Sync] Runtime - 1695 ms (57.50%), Memory - 19.3 MB (78.06%)

--- my-folder/0279-perfect-squares/solution.py
+++ my-folder/0279-perfect-squares/solution.py
@@ -1,16 +1,16 @@
 class Solution:
     def numSquares(self, n: int) -> int:
         
-        dp = [n + 1] * (n + 1)
-        dp[0] = 0
-        
-        nums = [num**2 for num in range(1, int(sqrt(n)) + 1)]
+        possibles = [num ** 2 for num in range(1, int(math.ceil(math.sqrt(n))) + 1 )]
 
-        for index in range(1, len(dp)):
-            for num in nums:
-                difference = index - num
+        dp = [ n + 1 ] * (n + 1)
+        dp[0] = 0
+
+        for index in range(len(dp)):
+            for possible in possibles:
+                difference = index - possible
                 if difference >= 0:
                     dp[index] = min(dp[index], 1 + dp[difference])
-
+        
         return dp[n]
 
