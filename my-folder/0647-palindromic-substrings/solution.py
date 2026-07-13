5049e63e934b4541aa0ac51affb058f95b117a0d
Daniel Li <110048127+BananaApache@users.noreply.github.com>
2026-07-13 17:59:24 +0000

    [LeetCode Sync] Runtime - 115 ms (79.89%), Memory - 19.3 MB (36.95%)

--- my-folder/0647-palindromic-substrings/solution.py
+++ my-folder/0647-palindromic-substrings/solution.py
@@ -1,51 +1,26 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
-
-        result = 0
-
-        for index in range(len(s)):
-            # odd
-            l, r = index, index
-            while l >= 0 and r < len(s) and s[l] == s[r]:
-                result += 1
-                l -= 1
-                r += 1
-
-            # even
-            l, r = index, index + 1
-            while l >= 0 and r < len(s) and s[l] == s[r]:
-                result += 1
-                l -= 1
-                r += 1
         
+        result = 0
+        for index in range(len(s)):
+
+            # odd case
+            left = index
+            right = index
+
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                result += 1
+                left -= 1
+                right += 1
+
+            # even case
+            left = index
+            right = index + 1
+
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                result += 1
+                left -= 1
+                right += 1
+
         return result
         
-        # def isP(left, right):
-        #     while left < right:
-        #         if s[left] != s[right]:
-        #             return False
-        #         left += 1
-        #         right -= 1
-            
-        #     return True
-
-        # result = 0
-        # seen = set()
-
-        # def dfs(left, right):
-        #     nonlocal result
-        #     # base case
-        #     if (left, right) not in seen and isP(left, right):
-        #         seen.add( (left, right) )
-        #         result += 1
-        #     if right >= len(s) - 1:
-        #         return
-
-        #     dfs(left + 1, right + 1)
-        #     dfs(left, right + 1)
-        
-        # dfs(0, 0)
-
-        # return result
-
-
