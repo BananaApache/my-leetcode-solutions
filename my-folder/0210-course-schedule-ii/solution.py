b7ec1575ade8cdad20f6d140a74ce00559fb0a61
Daniel Li <110048127+BananaApache@users.noreply.github.com>
2026-07-13 17:24:41 +0000

    [LeetCode Sync] Runtime - 4 ms (48.66%), Memory - 21.3 MB (19.27%)

--- my-folder/0210-course-schedule-ii/solution.py
+++ my-folder/0210-course-schedule-ii/solution.py
@@ -1,41 +1,35 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        
-        # build adj map
-        course2pres = {course : [] for course in range(numCourses)}
-        for course, pre in prerequisites:
-            course2pres[course].append(pre)
 
-        result = []
+        course2pre = defaultdict(list)
+        for course, pre in prerequisites:
+            course2pre[course].append(pre)
+        
         seen = set()
         cycle = set()
+        result = []
+
         def dfs(course):
             # base case
-            if course in seen:
+            if course in seen: # equal to course2pre[course] == []
                 return True
             if course in cycle:
                 return False
 
-            if course2pres[course] == []:
-                result.append(course)
-                seen.add(course)
-                return True
-
             cycle.add(course)
-            for pre in course2pres[course]:
+            for pre in course2pre[course]:
                 if not dfs(pre):
                     return False
             cycle.remove(course)
 
-            result.append(course)
             seen.add(course)
-            
+            result.append(course)
+
             return True
         
         for course in range(numCourses):
             if not dfs(course):
                 return []
-        
+            
         return result
-        
 
