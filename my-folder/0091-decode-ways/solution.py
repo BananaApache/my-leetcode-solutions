032cc341668b92aea4e0b45163e3fef7b22e8603
Daniel Li <110048127+BananaApache@users.noreply.github.com>
2026-07-16 19:00:44 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.1 MB (92.56%)

--- my-folder/0091-decode-ways/solution.py
+++ my-folder/0091-decode-ways/solution.py
@@ -1,28 +1,51 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
         
-        # state: index -> total decodings from index to end
-        # cache: index -> total decodings
+        # BOTTOM UP APPROACH
+        # state: index
+        # dfs(index): num decodings possible from index to end of s
+        # dp[index]: num decodings possible from index to end of s
+        # set dp[index] to (dp[index + 1] if s[index] valid) + (dp[index + 2] if s[index : index + 2] valid)
 
-        cache = {}
+        dp = [0] * len(s)
+        dp.append(1)
 
-        def dfs(index):
-            # base case
-            if index >= len(s):
-                return 1 # got to end successfully
-
+        for index in range(len(s) - 1, -1, -1):
             if s[index] == '0':
-                return 0
-            if index in cache:
-                return cache[index]
+                continue
             
-            decodings = dfs(index + 1)
+            dp[index] = dp[index + 1]
 
-            if index + 1 < len(s) and int(s[index : index + 2]) in range(10, 27):
-                decodings += dfs(index + 2)
+            if index + 2 <= len(s) and int(s[index : index + 2]) in range(10, 27):
+                dp[index] += dp[index + 2]
             
-            cache[index] = decodings
-            return decodings
+        return dp[0]
 
-        return dfs(0)
+        # TOP DOWN APPROACH
+        # state: index
+        # dfs(index): num decodings possible from index to end of s
+        # recurrence: try the index then [index:index + 1]. after do dfs(new index) of the ones that were valid
+        
+        # cache = {}
+
+        # def dfs(index):
+        #     # base case
+        #     if index == len(s):
+        #         return 1
+        #     if index in cache:
+        #         return cache[index]
+            
+        #     if s[index] == '0':
+        #         cache[index] = 0
+        #         return 0
+            
+        #     decodings = dfs(index + 1)
+
+        #     if index + 2 <= len(s) and int(s[index : index + 2]) in range(10, 27):
+        #         decodings += dfs(index + 2)
+
+        #     cache[index] = decodings
+        #     return decodings
+        
+        # return dfs(0)
 
