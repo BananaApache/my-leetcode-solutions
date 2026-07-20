5ca028311edb0bb8e0aec32b27e4499d45800f81
Daniel Li <110048127+BananaApache@users.noreply.github.com>
2026-07-20 15:51:17 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.4 MB (20.57%)

--- my-folder/0226-invert-binary-tree/solution.py
+++ my-folder/0226-invert-binary-tree/solution.py
@@ -8,18 +8,13 @@ class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         
         # base case
-        if root and (root.left is None and root.right is None): # either left not exist or right not exist
-            tmp = root.left
-            root.left = root.right
-            root.right = tmp
-            return root
+        if not root:
+            return None
+        
+        leftTree = self.invertTree(root.left)
+        rightTree = self.invertTree(root.right)
 
-        # non base case
-        if root:
-            tmpTree = self.invertTree(root.right)
-            root.right = self.invertTree(root.left)
-            root.left = tmpTree
+        root.left = rightTree
+        root.right = leftTree
 
         return root
-
-
