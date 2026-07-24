1bf8e44c49c3d9135bf54db86d0bc9d52943ebc2
Daniel Li <110048127+BananaApache@users.noreply.github.com>
2026-07-24 19:07:00 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 24.3 MB (5.04%)

--- my-folder/0113-path-sum-ii/solution.py
+++ my-folder/0113-path-sum-ii/solution.py
@@ -7,28 +7,29 @@
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
         
-        # information being passed down to children : new targetSum, current path
-        # dfs(root, target, path): explore root to current node and check for target and add to path accordingly
+        # child needs path information passed down from parent
+        # node knows if it is leaf or not
+        # dfs(node, runningSum, path): traverses down and keeps a total sum and the current path
+        # recurrence: node checks if self is leaf then appends to result if runningSum is target, else traverse children
 
         result = []
 
-        def dfs(root, target, path):
+        def dfs(root, runningSum, path):
             # base case
             if not root:
                 return
 
             path.append(root.val)
-            if not root.left and not root.right and root.val == target: # leaf node that is equal
-                result.append(path.copy())
-                path.pop()
+            runningSum += root.val
+            if not root.left and not root.right and runningSum == targetSum:
+                result.append(path)
                 return
-            
-            dfs(root.left, target - root.val, path)
-            dfs(root.right, target - root.val, path)
+            dfs(root.left, runningSum, path.copy())
+            dfs(root.right, runningSum, path.copy())
             path.pop()
 
             return
         
-        dfs(root, targetSum, [])
+        dfs(root, 0, [])
         return result
-        
+
