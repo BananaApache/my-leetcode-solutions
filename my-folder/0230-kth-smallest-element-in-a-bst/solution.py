86c556f699172c5bb9f32c527298219dbd6e4f0c
Daniel Li <110048127+BananaApache@users.noreply.github.com>
2026-07-24 20:03:23 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 22 MB (87.31%)

--- my-folder/0230-kth-smallest-element-in-a-bst/solution.py
+++ my-folder/0230-kth-smallest-element-in-a-bst/solution.py
@@ -6,25 +6,28 @@
 #         self.right = right
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-
-        answer = None
-        count = k
         
-        def dfs(root):
-            nonlocal count, answer
-            if root is None:
-                # we are at an end
-                return
-            
-            # keep going left
-            dfs(root.left)
-            # check root
-            if count == 1:
-                answer = root.val
-            count -= 1
-            # go right
-            dfs(root.right)
+        # minimum information is current index
+        # dfs(root): while going back up, updates index and will return when k == index the correct node
+        # left -> root -> right
 
-        dfs(root)
-        return answer
+        index = 0
+
+        def dfs(root):
+            nonlocal index
+            # base case
+            if not root:
+                return None
+            
+            left = dfs(root.left)
+            if left is not None:
+                return left
+            index += 1
+            if index == k:
+                return root.val
+            right = dfs(root.right)
+            if right is not None:
+                return right
+
+        return dfs(root)
 
