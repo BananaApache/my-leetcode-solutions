d224df56015838f6e388e811eb7439891cf8b4ee
Daniel Li <110048127+BananaApache@users.noreply.github.com>
2026-07-26 23:08:10 +0000

    [LeetCode Sync] Runtime - 2 ms (80.59%), Memory - 21.1 MB (64.46%)

--- my-folder/0105-construct-binary-tree-from-preorder-and-inorder-traversal/solution.py
+++ my-folder/0105-construct-binary-tree-from-preorder-and-inorder-traversal/solution.py
@@ -6,15 +6,44 @@
 #         self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        # base case
-        if not preorder or not inorder:
-            return None
+        
+        # pre: root, LEFT, RIGHT
+        # in : LEFT, root, RIGHT
 
-        root = TreeNode(preorder[0])
-        rootIndex = inorder.index(preorder[0])
+        # first in pre -> ROOT
+        # in: [left .. root .. right]
+        # size of left for inorder list and size of right for inorder list is useful information
+        # pre: [root .. size of left .. size of right]
 
-        root.left = self.buildTree(preorder[1 : rootIndex + 1], inorder[0 : rootIndex])
-        root.right = self.buildTree(preorder[rootIndex + 1 : ], inorder[rootIndex + 1 : ])
+        #             0 1 2 3 4 5
+        #             m             
+        # preorder = [3,9,20,15,7]
+        # inorder  = [9,3,15,20,7]
+        #             L m
 
-        return root
+        inorderMap = {}
+        for index in range(len(inorder)):
+            inorderMap[inorder[index]] = index
+
+        def build(preL, preR, inL, inR):
+            # base case
+            if preL == preR:
+                return None
+            if preR - preL == 1:
+                return TreeNode(preorder[preL])
+
+            root = TreeNode(preorder[preL])
+            rootIndex = inorderMap[preorder[preL]]
+
+            leftSize = rootIndex - inL
+            rightSize = inR - rootIndex
+
+            root.left = build( preL=preL + 1, preR=preL + leftSize + 1, inL=inL, inR=rootIndex )
+            root.right = build( preL=preL + leftSize + 1, preR=preR, inL=rootIndex + 1, inR=inR)
+            # root.left = build( preorder[ 1 : rootIndex + 1] , inorder[ 0 : rootIndex ] )
+            # root.right = build( preorder[ rootIndex + 1 : ] , inorder[ rootIndex + 1 : ] )
+
+            return root
+
+        return build(0, len(preorder), 0, len(preorder))
 
