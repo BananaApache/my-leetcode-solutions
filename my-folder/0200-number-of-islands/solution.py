e7e076ea804cd48225926c1bbf64cf9bee5e8e09
Daniel Li <110048127+BananaApache@users.noreply.github.com>
2026-07-28 19:27:13 +0000

    [LeetCode Sync] Runtime - 250 ms (42.26%), Memory - 21.4 MB (98.22%)

--- my-folder/0200-number-of-islands/solution.py
+++ my-folder/0200-number-of-islands/solution.py
@@ -1,28 +1,28 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        
+        # bfs(row, col): will run on only 1s and traverse until all those 1s are converted to 0
 
         rows, cols = len(grid), len(grid[0])
-        result = 0
-        
-        def bfs(startRow, startCol):
-            q = deque( [(startRow, startCol)] )
-            grid[startRow][startCol] = "0"
 
+        def bfs(startRow, startCol):
+            q = deque([ (startRow, startCol) ])
+            grid[startRow][startCol] = "0"
             while q:
                 row, col = q.popleft()
 
-                for addRow, addCol in [ [1,0],[0,1],[-1,0],[0,-1] ]:
+                for addRow, addCol in [ [0,1],[1,0],[-1,0],[0,-1] ]:
                     newRow, newCol = row + addRow, col + addCol
-
-                    if (newRow in range(rows) and newCol in range(cols)) and (grid[newRow][newCol] == "1"):
-                        q.append( (newRow, newCol) )
+                    if (0 <= newRow and newRow < rows) and (0 <= newCol and newCol < cols) and grid[newRow][newCol] == "1":
                         grid[newRow][newCol] = "0"
-
+                        q.append( (newRow, newCol) )
+        
+        result = 0
         for row in range(rows):
             for col in range(cols):
                 if grid[row][col] == "1":
-                    result += 1
                     bfs(row, col)
+                    result += 1
 
         return result
 
