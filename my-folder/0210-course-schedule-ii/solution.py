b8927f2860c962c8c08c8e4e84ae1b05ebc9e77d
Daniel Li <110048127+BananaApache@users.noreply.github.com>
2026-07-30 19:01:19 +0000

    [LeetCode Sync] Runtime - 8 ms (10.74%), Memory - 21.5 MB (6.61%)

--- my-folder/0210-course-schedule-ii/solution.py
+++ my-folder/0210-course-schedule-ii/solution.py
@@ -1,35 +1,40 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        
+        # node represents one course
+        # edge represents that node's required courses. course -> pre
+        # need to find courses to take before current course in order
+
+        # dfs: go down all prereqs until finding a good one, then take that and go back up
+        # recurrence: add course to output once all prereqs are taken
 
         course2pre = defaultdict(list)
         for course, pre in prerequisites:
             course2pre[course].append(pre)
-        
-        seen = set()
-        cycle = set()
-        result = []
 
+        output = []
+        taken = set()
+        cycle = set()
         def dfs(course):
             # base case
-            if course in seen: # equal to course2pre[course] == []
-                return True
             if course in cycle:
                 return False
-
+            if course in taken:
+                return True
+            
             cycle.add(course)
             for pre in course2pre[course]:
                 if not dfs(pre):
                     return False
             cycle.remove(course)
 
-            seen.add(course)
-            result.append(course)
-
+            taken.add(course)
+            output.append(course)
             return True
         
         for course in range(numCourses):
             if not dfs(course):
                 return []
-            
-        return result
+        
+        return output
 
