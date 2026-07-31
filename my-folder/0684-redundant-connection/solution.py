06dc1f115602639161f34e80c5c92df5785dfed4
Daniel Li <110048127+BananaApache@users.noreply.github.com>
2026-07-31 17:30:57 +0000

    [LeetCode Sync] Runtime - 11 ms (15.72%), Memory - 19.6 MB (51.20%)

--- my-folder/0684-redundant-connection/solution.py
+++ my-folder/0684-redundant-connection/solution.py
@@ -1,24 +1,27 @@
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
         
-        # union find for connected undirected graph
-        # treat edges as [parent, node]
-        # on each pair, run find on parent
-        # after that, check is parent is equal to root parent
-        # that means there is a cycle
+        # Which edge is the first one that connects two nodes that were already connected?
 
-        node2parent = { node : node for node in range(1,len(edges)+1) }
-
-        # find a node's super parent (root parent)
-        def find(node):
-            while node != node2parent[node]: # stop when parent = node2parent[node] (parent is its own parent)
-                node = node2parent[node]
-            return node
         
-        # unioning the pairs
+        # find parent of n1, parent of n2
+        # both are different -> merge
+        # both are same -> found a redundant connection
+        # 
+
+        parentMap = { node : node for node in range(1, len(edges)+1) }
+
+        # find: what is this nodes root parent
+        def find(node):
+            # stop when node is its own parent
+            while node != parentMap[node]:
+                node = parentMap[node]
+            return node
+
         for n1, n2 in edges:
             p1, p2 = find(n1), find(n2)
             if p1 == p2:
                 return [n1, n2]
-            node2parent[p2] = p1
+            else:
+                parentMap[p2] = p1
 
