af7aa24e5e804dcc17d84b292948f7067ed3a1f8
Daniel Li <110048127+BananaApache@users.noreply.github.com>
2026-08-02 23:56:39 +0000

    [LeetCode Sync] Runtime - 15 ms (84.30%), Memory - 19.4 MB (90.10%)

--- my-folder/0695-max-area-of-island/solution.py
+++ my-folder/0695-max-area-of-island/solution.py
@@ -1,26 +1,34 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         
-        # dfs(row, col): traverses 1s, sets them to 0s, returns its area
-
-        rows, cols = len(grid), len(grid[0])
-
-        def dfs(row, col):
-            # base case
-            if not (0 <= row < rows and 0 <= col < cols):
-                return 0
-            if grid[row][col] == 0:
-                return 0
-
-            grid[row][col] = 0
+        # bfs(row, col): traverses 1s only, updates to 0s, adds to some area, updates result at end
             
-            return 1 + dfs(row + 1, col) + dfs(row, col + 1) + dfs(row - 1, col) + dfs(row, col - 1)
-
+        rows, cols = len(grid), len(grid[0])
         result = 0
+        
+        def bfs(startRow, startCol):
+            nonlocal result
+            q = deque([ (startRow, startCol) ])
+            grid[startRow][startCol] = 0
+            area = 1
+
+            while q:
+                row, col = q.popleft()
+
+                for addRow, addCol in [ [1,0],[0,1],[-1,0],[0,-1] ]:
+                    newRow, newCol = row + addRow, col + addCol
+                    if 0 <= newRow < rows and 0 <= newCol < cols and grid[newRow][newCol] == 1:
+                        q.append( (newRow, newCol) )
+                        grid[newRow][newCol] = 0
+                        area += 1
+            
+            result = max(result, area)
+
         for row in range(rows):
             for col in range(cols):
                 if grid[row][col] == 1:
-                    result = max(result, dfs(row, col))
-            
+                    bfs(row, col)
+        
         return result
+
 
