f3acb6b90078aebf591ea9785a97f65cb1dc5390
Daniel Li <110048127+BananaApache@users.noreply.github.com>
2026-08-03 18:18:14 +0000

    [LeetCode Sync] Runtime - 3 ms (61.49%), Memory - 19.8 MB (28.17%)

--- my-folder/0684-redundant-connection/solution.py
+++ my-folder/0684-redundant-connection/solution.py
@@ -1,27 +1,31 @@
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
         
-        # Which edge is the first one that connects two nodes that were already connected?
+        parentMap = { node : node for node in range(1,len(edges)+1) }
+ 
+        # find function with halving path compression
+        # def find(node):
+        #     # will find a nodes root parent
+        #     while node != parentMap[node]:
+        #         parentMap[node] = parentMap[parentMap[node]] # sets a node parent to its grandparent
+        #         node = parentMap[node]
+        #     return node
 
-        
-        # find parent of n1, parent of n2
-        # both are different -> merge
-        # both are same -> found a redundant connection
-        # 
-
-        parentMap = { node : node for node in range(1, len(edges)+1) }
-
-        # find: what is this nodes root parent
+        # find function with recursive path compression
         def find(node):
-            # stop when node is its own parent
-            while node != parentMap[node]:
-                node = parentMap[node]
-            return node
+            # base case
+            if parentMap[node] == node:
+                return node
 
-        for n1, n2 in edges:
-            p1, p2 = find(n1), find(n2)
-            if p1 == p2:
-                return [n1, n2]
+            parentMap[node] = find(parentMap[node])
+            return parentMap[node]
+
+        # union part
+        for node1, node2 in edges:
+            parent1, parent2 = find(node1), find(node2)
+
+            if parent1 == parent2:
+                return [node1, node2]
             else:
-                parentMap[p2] = p1
+                parentMap[parent2] = parent1
 
