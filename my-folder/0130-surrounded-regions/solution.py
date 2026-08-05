bcb6dc4aaf4e9e9188d417319fd1133cf8da8f73
Daniel Li <110048127+BananaApache@users.noreply.github.com>
2026-08-05 01:03:06 +0000

    [LeetCode Sync] Runtime - 11 ms (19.45%), Memory - 22.3 MB (59.38%)

--- my-folder/0130-surrounded-regions/solution.py
+++ my-folder/0130-surrounded-regions/solution.py
@@ -3,33 +3,37 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
+        
+        # start at borders
+        # run dfs on the bordering 'O's and add each neighbor to border set
 
+        bordering = set()
         rows, cols = len(board), len(board[0])
-        visited = set()
+        def atBorder(row, col):
+            if row == 0 or row == rows - 1 or col == 0 or col == cols - 1:
+                return True
+            else:
+                return False
 
-        def isEdge(row, col):
-            return ((row == 0) or (row == rows - 1)) or ((col == 0) or (col == cols - 1))
-
-        def bfs(startRow, startCol):
-            q = deque([(startRow, startCol)])
-            visited.add( (startRow, startCol) )
-
-            while q:
-                row, col = q.popleft()
-
-                for addRow, addCol in [[1,0],[0,1],[-1,0],[0,-1]]:
-                    newRow, newCol = row + addRow, col + addCol
-                    if newRow in range(rows) and newCol in range(cols) and board[newRow][newCol] == 'O' and (newRow, newCol) not in visited:
-                        q.append( (newRow, newCol) )
-                        visited.add( (newRow, newCol) )
-
+        q = deque()
         for row in range(rows):
             for col in range(cols):
-                if board[row][col] == 'O' and isEdge(row, col):
-                    bfs(row, col)
+                if board[row][col] == 'O' and atBorder(row, col):
+                    q.append( (row, col) )
+                    bordering.add( (row, col) )
+        print(q)
+        
+        while q:
+            row, col = q.popleft()
 
+            for addRow, addCol in [ [1,0],[0,1],[-1,0],[0,-1] ]:
+                newRow, newCol = row+addRow, col+addCol
+                if 0<=newRow<rows and 0<=newCol<cols and board[newRow][newCol] == 'O' and (newRow, newCol) not in bordering:
+                    q.append( (newRow, newCol) )
+                    bordering.add( (newRow, newCol) )
+        
         for row in range(rows):
             for col in range(cols):
-                if (row, col) not in visited:
+                if (row, col) not in bordering:
                     board[row][col] = 'X'
 
