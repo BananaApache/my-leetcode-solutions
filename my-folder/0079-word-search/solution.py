5d9e7debb0fdc1da1def596213af64953f9f6104
Daniel Li <110048127+BananaApache@users.noreply.github.com>
2026-08-06 18:36:04 +0000

    [LeetCode Sync] Runtime - 4834 ms (16.94%), Memory - 19.4 MB (83.29%)

--- my-folder/0079-word-search/solution.py
+++ my-folder/0079-word-search/solution.py
@@ -1,32 +1,39 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-
-        rows = len(board)
-        cols = len(board[0])
         
-        def backtrack(row, col, wordIndex, seen):
+        # dfs will work because exploring one path deeply
+        # keep appending to current word until wrong letter then backtrack
+        # run dfs only on cells with first letter of word
+        # question is how to build seen set
+        # use different seen set for each possible start because word can loop back around
+
+        rows, cols = len(board), len(board[0])
+
+        # traverses path until cell doesnt match the letter of word at index
+        def dfs(index, row, col):
             # base case
-            if board[row][col] != word[wordIndex]:
+            if index == len(word) - 1:
+                if board[row][col] == word[index]:
+                    return True
+                else:
+                    return False
+            if board[row][col] != word[index]:
                 return False
-            if wordIndex == len(word) - 1:
-                return True
             
-            seen.add( (row, col) )
+            # seen.add( (row, col) )
+            board[row][col] = f"#bruh{board[row][col]}"
             for addRow, addCol in [ [1,0],[0,1],[-1,0],[0,-1] ]:
-                newRow, newCol = row + addRow, col + addCol
-                if newRow in range(rows) and newCol in range(cols) and (newRow, newCol) not in seen:
-                    if backtrack(newRow, newCol, wordIndex + 1, seen):
+                newRow, newCol = row+addRow, col+addCol
+                if 0<=newRow<rows and 0<=newCol<cols and not board[newRow][newCol].startswith("#"):
+                    if dfs(index+1, newRow, newCol):
                         return True
-            
-            seen.remove( (row, col) )
+            board[row][col] = board[row][col][5]
             return False
-            
+        
         for row in range(rows):
             for col in range(cols):
-                if board[row][col] == word[0]:
-                    seen = set()
-                    if backtrack(row, col, 0, seen):
-                        return True
-
+                if dfs(0, row, col):
+                    return True
+        
         return False
 
