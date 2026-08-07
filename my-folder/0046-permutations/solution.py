34cbbb24b497ddfa7fcbc63105c2daf4cb13a7b5
Daniel Li <110048127+BananaApache@users.noreply.github.com>
2026-08-07 14:22:33 +0000

    [LeetCode Sync] Runtime - 3 ms (31.08%), Memory - 19.7 MB (9.56%)

--- my-folder/0046-permutations/solution.py
+++ my-folder/0046-permutations/solution.py
@@ -1,20 +1,21 @@
-
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         
+        # dfs will take a current permutation and add to from the remaining
+
         result = []
 
-        def dfs(permutation, remaining):
+        def dfs(curr, remaining):
             # base case
-            if len(permutation) == len(nums):
-                result.append(permutation)
+            if len(curr) == len(nums):
+                result.append(curr)
                 return
-
+            
             for index in range(len(remaining)):
-                dfs(permutation + [remaining[index]], remaining[:index] + remaining[index + 1:])
-
-        for index in range(len(nums)):
-            dfs([nums[index]], nums[:index] + nums[index + 1:])
+                dfs(curr + [remaining[index]], remaining[:index]+remaining[index+1:])
+            return
         
+        for index in range(len(nums)):
+            dfs([nums[index]], nums[:index]+nums[index+1:])
         return result
 
