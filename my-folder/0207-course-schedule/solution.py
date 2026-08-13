7bc295854df75355d842c557b5e7385fc4ef7c2c
Daniel Li <110048127+BananaApache@users.noreply.github.com>
2026-08-13 17:37:43 +0000

    [LeetCode Sync] Runtime - 4 ms (64.58%), Memory - 21.3 MB (18.55%)

--- my-folder/0207-course-schedule/solution.py
+++ my-folder/0207-course-schedule/solution.py
@@ -1,38 +1,34 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         
-        # directed, could be unconnected components
-        # all nodes represent courses
-        # edges can represents this course needs its neighbor courses. course -> pre
-        # a cycle means it is impossible to take all courses
-        # detecting cycle return False
-        # dfs is more natural at detecting cycles
+        # [a, b] : take b -> take a
+        # [course, prerequisite]
 
-        # dfs() will answer if this course can be completed or not, or false if there is cycle
-        # recurrence: return whether or not all my neighor courses can be taken
+        # can make adjacencyMap from course to its prerequisites
 
-        course2pres = { course : [] for course in range(numCourses) }
+        course2pre = defaultdict(list)
         for course, pre in prerequisites:
-            course2pres[course].append(pre)
-        
+            course2pre[course].append(pre)
+
+        # dfs: keeps traversing until found False. will be True when course has no prerequisities, or finished all of its prerequisities
+
         cycle = set()
         def dfs(course):
             # base case
-            if course in cycle:
-                cycle.remove(course)
+            if course in cycle: # cycled course
                 return False
-
+            if course2pre[course] == []: # finished course
+                return True
+            
             cycle.add(course)
-            for pre in course2pres[course]:
+            for pre in course2pre[course]:
                 if not dfs(pre):
                     return False
             cycle.remove(course)
-            course2pres[course] = []
+            course2pre[course] = []
             return True
-
+        
         for course in range(numCourses):
             if not dfs(course):
                 return False
-        
         return True
-
