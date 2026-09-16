99305888faf4d9954511016c44289ceb545ec401
Daniel Li <110048127+BananaApache@users.noreply.github.com>
2026-09-16 19:21:58 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.5 MB (5.39%)

--- my-folder/0153-find-minimum-in-rotated-sorted-array/solution.py
+++ my-folder/0153-find-minimum-in-rotated-sorted-array/solution.py
@@ -1,27 +1,25 @@
 class Solution:
-    # def binarySearch(self, nums)
     def findMin(self, nums: List[int]) -> int:
         
-        result = nums[0]
+        # cases:
+        # LEFT > RIGHT
+        #   MID > RIGHT -> look right
+        #   MID < RIGHT -> look left
+        # LEFT < RIGHT -> return num at LEFT
 
         left = 0
         right = len(nums) - 1
-
+        result = nums[0]
         while left <= right:
-            if nums[left] < nums[right]:
+            mid = (left + right) // 2
+            if nums[left] > nums[right]:
+                if nums[mid] >= nums[left]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+                result = min(result, nums[mid])
+            else:
                 result = min(result, nums[left])
                 break
-            
-            middle = (left + right) // 2
-
-            if nums[middle] >= nums[left]:
-                # search right
-                result = min(result, nums[middle])
-                left = middle + 1
-            else:
-                # search left
-                result = min(result, nums[middle])
-                right = middle - 1
-
         return result
-        
+
