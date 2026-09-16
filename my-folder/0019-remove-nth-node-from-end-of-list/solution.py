9fc993be9434e5667a91a6d0a67314952602edba
Daniel Li <110048127+BananaApache@users.noreply.github.com>
2026-09-16 16:10:48 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.3 MB (25.80%)

--- my-folder/0019-remove-nth-node-from-end-of-list/solution.py
+++ my-folder/0019-remove-nth-node-from-end-of-list/solution.py
@@ -4,72 +4,45 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-
-        # # edge cases
-        # if head.next is None: # one node, n has to be 1, return empty
-        #     return None
+    def removeNthFromEnd(self, head: ListNode | None, n: int) -> ListNode | None:
         
-        # slow = head
-        # fast = head
-        # count = 0
-        
-        # # stop when fast is null or fast.next is null -> fast not null and fast.next not null
-        # while fast is not None and fast.next is not None:
-        #     slow = slow.next
-        #     fast = fast.next.next
-
-        #     count += 1
-
-        # if fast is None:
-        #     length = count*2 + 1
-        # else:
-        #     length = count*2 + 2
-
-        # position = length - n - 1
-        # print(position)
-
-        # # position - 1 = 2
-        # # count = 2
-        # # 1,2,3,4,5
-        # #     1
-
-        # if position == 0:
-        #     return head.next
-
-        # count = 0
         # node = head
-        # while count < position - 1:
+        # length = 0
+        # while node:
         #     node = node.next
-        #     count += 1
+        #     length += 1
+        # goal = length - n + 1
 
-        # print(node.val)
+        # # get prev node
+        # index = 1
+        # node = head
+        # prev = None
+        # while index != goal:
+        #     prev = node
+        #     node = node.next
+        #     index += 1
         
-        # if node.next and node.next.next is None:
-        #     node.next = None
+        # if prev:
+        #     prev.next = node.next
+        #     return head
         # else:
-        #     node.next = node.next.next
-
-        # return head
-
-        if head.next is None: # one node, n has to be 1, return empty
-            return None
-
-        dummy = ListNode(0, next=head)
-
-        left = dummy
-        right = head
-        count = n
-        while count > 0 and right:
-            right = right.next
-            count -= 1
-
-        while right:
-            left = left.next
-            right = right.next
-
-        left.next = left.next.next
-
-        return dummy.next
+        #     return head.next
         
+        hashmap = {}
+        index = 0
+        node = head
+        while node:
+            hashmap[index] = node
+            node = node.next
+            index += 1
+        
+        length = len(hashmap)
+        prev = length - n - 1
+        goal = length - n
+
+        if prev >= 0:
+            hashmap[prev].next = hashmap[goal].next
+            return head
+        else:
+            return head.next
 
