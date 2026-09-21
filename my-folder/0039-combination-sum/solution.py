82a29da99eb786efad7b607dcc3db70ad96a9959
Daniel Li <110048127+BananaApache@users.noreply.github.com>
2026-09-21 16:46:57 +0000

    [LeetCode Sync] Runtime - 7 ms (73.35%), Memory - 19.5 MB (66.43%)

--- my-folder/0039-combination-sum/solution.py
+++ my-folder/0039-combination-sum/solution.py
@@ -1,19 +1,38 @@
 class Solution:
-    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+    def combinationSum(self, candidates: list[int], target: int) -> list[list[int]]:
         
+        # dfs and backtracking
+        # append to current when going down
+        # pop when going back up
+
+        # decisions are the candidates at each level
+
         result = []
 
-        def dfs(total, path, index):
+        #  0 1 2
+        # [2,3,5]
+        # target = 8
+
+
+        def dfs(curr, total, index):
+            # base case
+            if total > target:
+                return
             if total == target:
-                result.append(path)
-                return
-            if index >= len(candidates) or total > target:
+                result.append(curr.copy())
                 return
 
-            dfs(total + candidates[index], path + [candidates[index]], index)
-            dfs(total, path, index + 1)
-
-        dfs(0, [], 0)
-
+            for newIndex in range(len(candidates[index : ])):
+                curr.append(candidates[newIndex + index])
+                dfs(curr, total + candidates[newIndex + index], index + newIndex)
+                curr.pop()
+        
+        dfs([], 0, 0)
+        # curr = []
+        # for index in range(len(candidates)):
+        #     curr.append(candidates[index])
+        #     dfs(curr, candidates[index], index)
+        #     curr.pop()
+            
         return result
 
